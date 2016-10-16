9a0b65322190a76cda1a4d8df7a4f3d71ef042fc
Steve Lee <seung.jin.lee@mail.mcgill.ca>
2016-10-16 18:09:10 -0400

    Start light localizer
    begin part 2 of lab 4

--- src/localization/Lab4.java
+++ src/localization/Lab4.java
@@ -38,9 +38,9 @@ public class Lab4 {
 		// 3. Create a sample provider instance for the above and initialize operating mode
 		// 4. Create a buffer for the sensor data
 		
-//		SensorModes colorSensor = new EV3ColorSensor(colorPort);
-//		SampleProvider colorValue = colorSensor.getMode("Red");			// colorValue provides samples from this instance
-//		float[] colorData = new float[colorValue.sampleSize()];			// colorData is the buffer in which data are returned
+		SensorModes colorSensor = new EV3ColorSensor(colorPort);
+		SampleProvider colorValue = colorSensor.getMode("Red");			// colorValue provides samples from this instance
+		float[] colorData = new float[colorValue.sampleSize()];			// colorData is the buffer in which data are returned
 				
 		// setup the odometer and display
 		Odometer odo = new Odometer(leftMotor, rightMotor, 30, true);
@@ -59,8 +59,8 @@ public class Lab4 {
 		
 		
 		// perform the light sensor localization
-//		LightLocalizer lsl = new LightLocalizer(odo, colorValue, colorData);
-//		lsl.doLocalization();			
+		LightLocalizer lsl = new LightLocalizer(odo, nav, colorValue, colorData, rightMotor, leftMotor);
+		lsl.doLocalization();			
 		
 		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
 		System.exit(0);	
--- src/localization/LightLocalizer.java
+++ src/localization/LightLocalizer.java
@@ -1,18 +1,28 @@
 package localization;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.robotics.SampleProvider;
 
 public class LightLocalizer {
 	private Odometer odo;
+	private Navigation nav;
 	private SampleProvider colorSensor;
-	private float[] colorData;	
-	
-	public LightLocalizer(Odometer odo, SampleProvider colorSensor, float[] colorData) {
+	private float[] colorData;
+	private EV3LargeRegulatedMotor leftMotor, rightMotor;
+
+	public LightLocalizer(Odometer odo, Navigation nav, SampleProvider colorSensor, float[] colorData,
+			EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor) {
 		this.odo = odo;
 		this.colorSensor = colorSensor;
 		this.colorData = colorData;
+		this.nav = nav;
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		
+		this.leftMotor.setAcceleration(nav.ACCELERATION);
+		this.rightMotor.setAcceleration(nav.ACCELERATION);
 	}
-	
+
 	public void doLocalization() {
 		// drive to location listed in tutorial
 		// start rotating and clock all 4 gridlines
